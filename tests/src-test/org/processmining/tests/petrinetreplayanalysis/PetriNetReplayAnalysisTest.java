5f5a8dc63f2433d8c36c36d0417755a6f97409d8
imtesiu <imatesiu@gmail.com>
2011-12-08 21:31:27 +0100

    Add and fix test case

--- tests/src-test/org/processmining/tests/petrinetreplayanalysis/PetriNetReplayAnalysisTest.java
+++ tests/src-test/org/processmining/tests/petrinetreplayanalysis/PetriNetReplayAnalysisTest.java
@@ -7,13 +7,13 @@ import org.processmining.contexts.test.PromTest;
 public class PetriNetReplayAnalysisTest extends PromTest {
 	
 	@Test
-	  public void testBPMN1() throws Throwable {
+	  public void testPetrinetReplayAnalysis1() throws Throwable {
 	    String args[] = new String[] {"-l"};
 	    CLI.main(args);
 	  }
 
 	  @Test
-	  public void testBPMN2() throws Throwable {
+	  public void testPetrinetReplayAnalysis2() throws Throwable {
 	    String testFileRoot = System.getProperty("test.testFileRoot", PromTest.defaultTestDir);
 	    String args[] = new String[] {"-f", testFileRoot+"/PetriNetsReplayAnalysis_Example.txt"};
 	    
