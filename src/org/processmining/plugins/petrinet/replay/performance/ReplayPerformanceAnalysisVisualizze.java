1bf2d66d7d14c3090ba5582da61a069d2bd45bbb
spagnolo <spagnolo@a68f48d4-8d00-424f-b812-3f22a534a02a>
2011-12-09 01:22:04 +0000

    fix condition visualizzer
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/PetriNetReplayAnalysis/Trunk@4120 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/petrinet/replay/performance/ReplayPerformanceAnalysisVisualizze.java
+++ src/org/processmining/plugins/petrinet/replay/performance/ReplayPerformanceAnalysisVisualizze.java
@@ -6,8 +6,10 @@ package org.processmining.plugins.petrinet.replay.performance;
 import javax.swing.JComponent;
 
 import org.deckfour.xes.model.XLog;
+import org.processmining.contexts.uitopia.UIPluginContext;
 import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
 import org.processmining.contexts.uitopia.annotations.Visualizer;
+import org.processmining.contexts.util.StringVisualizer;
 import org.processmining.framework.connections.ConnectionCannotBeObtained;
 import org.processmining.framework.plugin.PluginContext;
 import org.processmining.framework.plugin.Progress;
@@ -26,7 +28,7 @@ public class ReplayPerformanceAnalysisVisualizze {
 	@PluginVariant(requiredParameterLabels = { 0 })
 	@UITopiaVariant(affiliation = "Department of Computer Science University of Pisa", author = "R.Guanciale,G.Spagnolo et al.", email = "spagnolo@di.unipi.it", pack = "PetriNetReplayAnalysis")
 	public JComponent visualize(PluginContext context, TotalPerformanceResult tovisualize) {
-	
+		if(context instanceof UIPluginContext){
 			try {
 				ReplayAnalysisConnection connection = context.getConnectionManager().getFirstConnection(
 						ReplayAnalysisConnection.class, context, tovisualize);
@@ -43,7 +45,10 @@ public class ReplayPerformanceAnalysisVisualizze {
 				context.log("Connection does not exist", MessageLevel.DEBUG);
 				return null;
 			}
-		
+		}else {
+
+			return StringVisualizer.visualize(context, tovisualize.toString());
+		}
 
 	}
 
